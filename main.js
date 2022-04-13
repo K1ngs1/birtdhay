9088bb1e7312110950ad9755e6ace1e91697cb35
K1ngs1 <76851907+K1ngs1@users.noreply.github.com>
2022-04-13 16:54:35 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function preload(){
+}
+
+function setup(){
+    canvas = createCanvas(640, 480);
+    canvas.position(100, 200);
+    video = createCapture(VIDEO);
+    circle(100, 100, 200)
+    ellipse(300, 100, 55, 55);
+    rect(400, 10, 55, 55);
+}
